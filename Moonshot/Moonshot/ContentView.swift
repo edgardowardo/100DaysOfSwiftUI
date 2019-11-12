549a81d9d1a4dcf49e6523f76b8ba23b82aa69b1
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-12 12:40:15 +0000

    Codable astronauts

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,9 +8,32 @@
 
 import SwiftUI
 
+extension Bundle {
+    func decode(_ file: String) -> [Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loaded = try? decoder.decode([Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loaded
+    }
+}
+
 struct ContentView: View {
+    
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        Text("Hello, World!")
+        Text("\(astronauts.count)")
     }
 }
 
---
+++ Moonshot/Moonshot/Models/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by EDGARDO AGNO on 12/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
