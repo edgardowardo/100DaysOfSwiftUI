b63591dd9fa48aa52f0b42cc3f29c0460ab24e5c
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-16 11:40:38 +0000

    Cupcake form

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,24 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by EDGARDO AGNO on 16/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: Order())
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by EDGARDO AGNO on 16/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    @Published var type = 0
+    @Published var quantity = 3
+
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
