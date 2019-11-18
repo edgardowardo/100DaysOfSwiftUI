d37d433b3b2a5c149b62fb26c507713e7d86003f
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-18 12:44:53 +0000

    Harry potter database

--- HarryPotterDB/HarryPotterDB/ContentView.swift
+++ HarryPotterDB/HarryPotterDB/ContentView.swift
@@ -7,10 +7,33 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    @Environment(\.managedObjectContext) var moc
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+               List {
+                   ForEach(students, id: \.id) { student in
+                       Text(student.name ?? "Unknown")
+                   }
+               }
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+
+                    let student = Student(context: self.moc)
+                    student.id = UUID()
+                    student.name = "\(chosenFirstName) \(chosenLastName)"
+                    try? self.moc.save()
+                }
+           }
     }
 }
 
