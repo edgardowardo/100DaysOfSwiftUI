2060994bfcac8ee7d48263b2812b36621c669771
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-18 12:59:32 +0000

    Adds book

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct AddBookView: View {
     
     @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
     
     @State private var title = ""
     @State private var author = ""
@@ -46,7 +47,15 @@ struct AddBookView: View {
 
                 Section {
                     Button("Save") {
-                        // add the book
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+
+                        try? self.moc.save()
+                        self.presentationMode.wrappedValue.dismiss()
                     }
                 }
             }
