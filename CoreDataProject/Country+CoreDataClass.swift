daefcb6bff2d3a12c31401b88f3062e00dc10e44
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-20 14:01:10 +0000

    Candy and Country

---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by EDGARDO AGNO on 20/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
