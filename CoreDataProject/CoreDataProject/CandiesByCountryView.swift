16b078264ee16cc640a71c498da612cc7c013e70
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-21 10:28:36 +0000

    Revert

--- CoreDataProject/CoreDataProject/CandiesByCountryView.swift
+++ CoreDataProject/CoreDataProject/CandiesByCountryView.swift
@@ -24,34 +24,36 @@ struct CandiesByCountryView: View {
                     }
                 }
             }
-
+            
             Button("Add") {
-                let country1 = Country(context: self.moc)
-                country1.shortName = "UK"
-                country1.fullName = "United Kingdom"
-
                 let candy1 = Candy(context: self.moc)
                 candy1.name = "Mars"
-                candy1.origin = country1
+                candy1.origin = Country(context: self.moc)
+                candy1.origin?.shortName = "UK"
+                candy1.origin?.fullName = "United Kingdom"
 
                 let candy2 = Candy(context: self.moc)
                 candy2.name = "KitKat"
-                candy2.origin = country1
+                candy2.origin = Country(context: self.moc)
+                candy2.origin?.shortName = "UK"
+                candy2.origin?.fullName = "United Kingdom"
 
                 let candy3 = Candy(context: self.moc)
                 candy3.name = "Twix"
-                candy3.origin = country1
-                
-                let country2 = Country(context: self.moc)
-                country2.shortName = "CH"
-                country2.fullName = "Switzerland"
-                
+                candy3.origin = Country(context: self.moc)
+                candy3.origin?.shortName = "UK"
+                candy3.origin?.fullName = "United Kingdom"
+
                 let candy4 = Candy(context: self.moc)
                 candy4.name = "Toblerone"
-                candy4.origin = country2
+                candy4.origin = Country(context: self.moc)
+                candy4.origin?.shortName = "CH"
+                candy4.origin?.fullName = "Switzerland"
 
                 try? self.moc.save()
             }
+            
+
         }
         .navigationBarTitle("Candies by Country", displayMode: .inline)
     }
