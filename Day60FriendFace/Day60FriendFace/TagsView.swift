8a9b3d90f343ba9031c4f87ca666d7a946d8d4d3
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-22 15:19:42 +0000

    Tags file

---
+++ Day60FriendFace/Day60FriendFace/TagsView.swift
@@ -0,0 +1,21 @@
+//
+//  TagsView.swift
+//  Day60FriendFace
+//
+//  Created by EDGARDO AGNO on 22/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagsView()
+    }
+}
