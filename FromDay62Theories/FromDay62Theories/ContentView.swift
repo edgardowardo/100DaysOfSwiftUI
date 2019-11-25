8e6c10583a256ee03dd6eb06d519256d6f8943c1
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-25 14:35:57 +0000

    From Day 62 theories

---
+++ FromDay62Theories/FromDay62Theories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FromDay62Theories
+//
+//  Created by EDGARDO AGNO on 25/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
