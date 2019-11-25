25ac74f2624615f60ec6d17785d9e64d586867fc
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-25 16:03:19 +0000

    ImagePickerSample

---
+++ FromDay62Theories/FromDay62Theories/Day63/ImagePickerSampleView.swift
@@ -0,0 +1,21 @@
+//
+//  ImagePickerSampleView.swift
+//  FromDay62Theories
+//
+//  Created by EDGARDO AGNO on 25/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePickerSampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerSampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerSampleView()
+    }
+}
