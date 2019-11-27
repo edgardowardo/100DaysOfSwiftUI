4d547d117b01a57c85c4096b6687ad83557c6f06
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-27 11:30:50 +0000

    Coordinator explained

--- FromDay62Theories/FromDay62Theories/ContentView.swift
+++ FromDay62Theories/FromDay62Theories/ContentView.swift
@@ -12,10 +12,10 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Day 62")) {
-                    NavigationLink("Custom binding", destination: CustomBindingView())
-                    NavigationLink("Action sheet", destination: ActionSheetSampleView())
+                Section(header: Text("Day 64")) {
+                    NavigationLink("Coordinator explained", destination: CoordinatorExplainedView())
                 }
+
                 Section(header: Text("Day 63")) {
                     NavigationLink("Image filter (none)", destination: ImageFiltersSampleView(filter: .none))
                     NavigationLink("Image filter (sepia)", destination: ImageFiltersSampleView(filter: .sepia))
@@ -23,6 +23,10 @@ struct ContentView: View {
                     NavigationLink("Image filter (crystalize)", destination: ImageFiltersSampleView(filter: .crystalize))
                     NavigationLink("Image filter (twirl)", destination: ImageFiltersSampleView(filter: .twirl))
                     NavigationLink("Image picker View Representable wrapper", destination: ImagePickerSampleView())
+                }
+                Section(header: Text("Day 62")) {
+                    NavigationLink("Custom binding", destination: CustomBindingView())
+                    NavigationLink("Action sheet", destination: ActionSheetSampleView())
                 }
             }
             .navigationBarTitle("> 62")
---
+++ FromDay62Theories/FromDay62Theories/Day64/CoordinatorExplainedView.swift
@@ -0,0 +1,21 @@
+//
+//  CoordinatorExplainedView.swift
+//  FromDay62Theories
+//
+//  Created by EDGARDO AGNO on 27/11/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import SwiftUI
+
+struct CoordinatorExplainedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoordinatorExplainedView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoordinatorExplainedView()
+    }
+}
