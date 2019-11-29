1fd62921c895ddc93593c384a586bc557ac40dee
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-29 18:07:54 +0000

    Pie shape

--- PieShape/PieShape/ContentView.swift
+++ PieShape/PieShape/ContentView.swift
@@ -8,9 +8,34 @@
 
 import SwiftUI
 
+extension CGRect {
+    var radius: CGFloat {
+        return min(size.width, size.height) / 2
+    }
+
+    var center: CGPoint {
+        return .init(x: midX, y: midY)
+    }
+}
+
+struct PieShape: Shape {
+
+    let degrees: Double
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: rect.center)
+        let startAngle = -degrees - 22.5
+        path.addRelativeArc(center: rect.center, radius: rect.radius, startAngle: Angle(degrees: startAngle), delta: Angle(degrees: 45))
+        path.closeSubpath()
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        PieShape(degrees: 360)
+            .stroke(lineWidth: 1)
     }
 }
 
