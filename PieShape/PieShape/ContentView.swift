4e6b550e4f81501382c466c7b4d8a3f67e9f929c
Edgardo Agno <edgardo.agno@gmail.com>
2019-11-30 10:17:48 +0000

    Arc shape

--- PieShape/PieShape/ContentView.swift
+++ PieShape/PieShape/ContentView.swift
@@ -32,10 +32,48 @@ struct PieShape: Shape {
     }
 }
 
+struct ArcShape: Shape {
+
+    let degrees: Double
+    var animationProgress: Double
+        
+    var animatableData: Double {
+        get { return animationProgress }
+        set { animationProgress = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let span = 22.5 //45.0
+        var path = Path()
+        let startAngle = -degrees - span / 2
+        let endAngle = -degrees + span / 2
+        path.addArc(center: rect.center, radius: rect.radius * 2 * CGFloat(animationProgress), startAngle: Angle(degrees: startAngle), endAngle: Angle(degrees: endAngle), clockwise: false)
+        return path
+    }
+}
+
 struct ContentView: View {
+    
+    @State var animationProgress = 0.0
+    
     var body: some View {
-        PieShape(degrees: 360)
-            .stroke(lineWidth: 1)
+        ZStack {
+            ArcShape(degrees: 45, animationProgress: animationProgress)
+                .stroke(lineWidth: 1)
+                .animation(
+                    Animation
+                        .easeOut
+                        .speed(0.5)
+                        .repeatForever(autoreverses: false))
+                .onAppear {
+                    withAnimation {
+                        self.animationProgress = 1.0
+                    }
+            }
+            
+            PieShape(degrees: 180)
+                .stroke(lineWidth: 1)
+        }
     }
 }
 
