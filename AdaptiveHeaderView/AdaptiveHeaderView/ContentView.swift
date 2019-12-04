aefa19687b27e1d743453d68eb72eefe24affc3b
Edgardo Agno <edgardo.agno@gmail.com>
2019-12-04 13:16:10 +0000

    No joy

--- AdaptiveHeaderView/AdaptiveHeaderView/ContentView.swift
+++ AdaptiveHeaderView/AdaptiveHeaderView/ContentView.swift
@@ -8,9 +8,51 @@
 
 import SwiftUI
 
+struct Hour: Identifiable, Hashable {
+    let id: Int
+    let hour: String
+}
+
 struct ContentView: View {
+    
+//    let hours = ["20", "21", "22", "23", "00", "01", "02", "03", "04"].enumerated()
+    let hours = [
+        Hour(id: 0, hour: "20"),
+        Hour(id: 1, hour: "21"),
+        Hour(id: 2, hour: "22"),
+        Hour(id: 3, hour: "23"),
+        Hour(id: 4, hour: "00"),
+        Hour(id: 5, hour: "01"),
+        Hour(id: 6, hour: "02"),
+        Hour(id: 7, hour: "03")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ZStack {
+                ForEach(hours, id: \.self) { hh in
+                    Text(self.day(of: hh.hour))
+//                        .offset(x: hh.id * 20)
+//                        .offset(x: 20)
+                }
+            }
+            
+            HStack {
+                ForEach(hours, id: \.self) {
+                    Text("\($0.hour)")
+                }
+            }
+        }
+    }
+    
+    func day(of hour: String) -> String {
+        if hour == "20" {
+            return "TODAY"
+        } else if hour == "00" {
+            return "Thu, 05 Dec"
+        } else {
+            return ""
+        }
     }
 }
 
